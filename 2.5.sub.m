90883006771488a1714d359e1a7647bd9cbe964d
Franz Webersberger <franzwebersberger@users.noreply.github.com>
2014-11-03 06:48:14 +0100

    replaced fig-4,fig-5

--- 2.5.sub.m
+++ 2.5.sub.m
@@ -10,8 +10,18 @@ E=zeros(1,m);
 for i=1:m
 	E(i)=norm(P(i,:)-Ptest)/norm(P(i,:));
 end;
+[emin,ix]=min(E);
 
 figure(1)
-subplot(1,2,1), pcolor(flipud(reshape(Reco,h,w))), shading interp, colormap(gray);
-subplot(1,2,2), bar(E), axis([0 m+1 0 1]);
+subplot(1,4,1), pcolor(flipud(reshape(Ctest,h,w))), shading interp, colormap(gray);
+set(gca,'Xtick',[],'Ytick',[]);
+title("test image");
+subplot(1,4,2), bar(E), axis([0 m+1 0 1]);
+title("errors");
+subplot(1,4,3), pcolor(flipud(reshape(Reco,h,w))), shading interp, colormap(gray);
+set(gca,'Xtick',[],'Ytick',[]);
+title("ev space proj");
+subplot(1,4,4), pcolor(flipud(reshape(A(ix,:),h,w))), shading interp, colormap(gray);
+set(gca,'Xtick',[],'Ytick',[]);
+title("best match");
 
